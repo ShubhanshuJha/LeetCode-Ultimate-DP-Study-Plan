d79e1ec941b4c399bc334684c53637237544ca6f
Shubhanshu Jha <52618335+ShubhanshuJha@users.noreply.github.com>
2022-01-23 01:12:56 +0530

    Create Fibonacci Number.java

---
+++ Day 1/Fibonacci Number.java
@@ -0,0 +1,18 @@
+/*The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n). */
+
+class Solution {
+    public int fib(int n) {
+        if (n <= 1) return n;
+        
+        int[] fib = new int[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
+    }
+}
