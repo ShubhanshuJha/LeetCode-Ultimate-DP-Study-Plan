bc19454b4ef8e5ffc2d86c2cff330dabc22dd4bc
Shubhanshu Jha <52618335+ShubhanshuJha@users.noreply.github.com>
2022-02-06 22:36:49 +0530

    Update Matrix Block Sum.java

--- Day 14/Matrix Block Sum.java
+++ Day 14/Matrix Block Sum.java
@@ -21,3 +21,36 @@ class Solution {
         return ans;
     }
 }
+
+
+// Optimized approach
+class Solution {    
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] ans = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int x = -k; x <= k; x++) {
+                    if (i + x >= 0 && i + x < m) {
+                        int l = Integer.max(j - k, 0);
+                        int r = j + k + 1;
+                        ans[i + x][l] += mat[i][j];
+                        if (r < n)
+                            ans[i + x][r] -= mat[i][j];
+                    }
+                }
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < m; i++) {
+            sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += ans[i][j];
+                ans[i][j] = sum;
+            }
+        }
+        return ans;
+    }
+}
