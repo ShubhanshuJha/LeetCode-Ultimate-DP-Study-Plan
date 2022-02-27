20aa0cca0efdd2df1ea1374bd0ff19f1071e6fbf
Shubhanshu Jha <52618335+ShubhanshuJha@users.noreply.github.com>
2022-02-28 00:40:49 +0530

    Update Longest Palindromic Substring.java

--- Day 17/Longest Palindromic Substring.java
+++ Day 17/Longest Palindromic Substring.java
@@ -19,3 +19,32 @@ class Solution {
         return res;
     }
 }
+
+// Optimized approach
+class Solution {
+    public String longestPalindrome(String s) {
+        String res = "";
+        int resLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // Odd Length
+            int l = i, r = i;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                if ((r - l + 1) > resLen) {
+                    res = s.substring(l, r + 1);
+                    resLen = r - l + 1;
+                }
+                l--; r++;
+            }
+            // Even Length
+            l = i; r = i + 1;
+            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+                if ((r - l + 1) > resLen) {
+                    res = s.substring(l, r + 1);
+                    resLen = r - l + 1;
+                }
+                l--; r++;
+            }
+        }
+        return res;
+    }
+}
